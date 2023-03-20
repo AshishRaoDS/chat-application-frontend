2b36cb0241074c959810dfc2c0be910ee5548a53
AshishRaoDS <ashraods@gmail.com>
2023-03-20 23:09:18 +0530

    feat: adding the form element for anonymous chat and socket.io client side implementation

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,10 @@
 "use client"
 import React, { useEffect, useState } from "react";
 import { io, Socket } from "socket.io-client";
-import styles from './page.module.css'
 
 type Props = {};
 
-const Home: React.FC<Props> = ({}) => {
+const Button: React.FC<Props> = ({}) => {
   const [nameState, setNameState] = useState("");
   const [messageState, setMessageState] = useState("");
   const [socket, setSocket] = useState<Socket>();
@@ -34,11 +33,7 @@ const Home: React.FC<Props> = ({}) => {
   };
 
   useEffect(() => {
-    const newSocket = io("http://localhost:3000", {
-      query: {
-        username: 'john_doe'
-      }
-    });
+    const newSocket = io("http://localhost:3000");
 
     newSocket.connect();
     newSocket.on("connect", () => {
@@ -59,27 +54,7 @@ const Home: React.FC<Props> = ({}) => {
 
   return (
     <>
-    <div className={styles.pageWrapper}>
-      <div className={styles.card}>
-        <h1>Welcome to your general chat site</h1>
-        <p>Where anonymity is valued the most</p>
-        <div className={styles.inputContainer}>
-          <input 
-          type="text" 
-          placeholder="Enter to secret name here" 
-          value={nameState} 
-          className={styles.input}
-          onChange={inputChangeHandler}
-          />
-          <button 
-          type="button" 
-          className={styles.submitNameCta}
-          onClick={submitNameHandler}
-          >Submit your name</button>
-        </div>
-      </div>
-    </div>
-      {/* <input type="text" value={nameState} onChange={inputChangeHandler} />
+      <input type="text" value={nameState} onChange={inputChangeHandler} />
       <button type="button" onClick={submitNameHandler}>
         Submit Name
       </button>
@@ -91,9 +66,9 @@ const Home: React.FC<Props> = ({}) => {
         <button type="button" onClick={submitMessage}>
           Submit Message
         </button>
-      </div> */}
+      </div>
     </>
   );
 };
 
-export default Home;
+export default Button;
